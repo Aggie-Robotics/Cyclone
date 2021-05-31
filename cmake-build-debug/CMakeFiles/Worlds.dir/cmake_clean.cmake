9edd1c76d98142b4133a6afa5bede9bd8b68b581
Aaron Quigg <quiggaf@gmail.com>
2021-05-31 18:40:43 -0500

    int commit

---
+++ cmake-build-debug/CMakeFiles/Worlds.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Worlds.dir/src/main.cpp.obj"
+  "libWorlds.a"
+  "libWorlds.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Worlds.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
